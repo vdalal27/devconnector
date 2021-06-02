2dd2fb76c8af4b83d915a4f4d87e10f5dc94c691
Vandana Dalal <vanu@Vandanas-MacBook-Pro.local>
2021-06-02 16:02:41 +0530

    updated users with post method

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-// @route GET api/users
+// @route POST api/users
 // @desc Test route
 // @access Public
-router.get('/', (req, res) => res.send('User route'));
+router.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('User Route');
+});
 
 module.exports = router;
