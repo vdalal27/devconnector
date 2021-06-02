f3d2743984d839875841638b71cf42b05490649f
Vandana Dalal <vanu@Vandanas-MacBook-Pro.local>
2021-06-02 15:47:53 +0530

    database connected, api added, user model created

--- config/db.js
+++ config/db.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = require('mongoURI');
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db);
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
     console.log('mongo db connected');
   } catch (err) {
     console.error(err.message);
