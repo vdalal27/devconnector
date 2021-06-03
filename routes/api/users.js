4a709fa0188d155d157cf30ba03d4503ac2cad9f
Vandana Dalal <vanu@Vandanas-MacBook-Pro.local>
2021-06-03 10:25:26 +0530

    user registration check added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
 // @route POST api/users
 // @desc Test route
 // @access Public
-router.post('/', (req, res) => {
-  console.log(req.body);
-  res.send('User Route');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Password should be 6 or more characters').isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
+    res.send('User Route');
+  }
+);
 
 module.exports = router;
