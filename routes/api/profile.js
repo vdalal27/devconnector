4be70d908001cd0907cc038c66b4681148dfecc4
Vandana Dalal <vanu@Vandanas-MacBook-Pro.local>
2021-06-09 15:13:04 +0530

    delete profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -211,4 +211,24 @@ router.put(
   }
 );
 
+// @route put api/profile/experience/:exp_id
+// @desc Delete profile experience
+// @access Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get index of exp
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id);
+    profile.experience.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
