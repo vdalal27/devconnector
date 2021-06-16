14def938daea432374adb5c315b08bc84bacff84
Vandana Dalal <vanu@Vandanas-MacBook-Pro.local>
2021-06-16 16:05:37 +0530

    redux alerts and store initiated

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,22 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import './App.css';
 
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className='container' />
-      <Route exact path='/register' component={Register} />
-      <Route exact path='/login' component={Login} />
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container' />
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
