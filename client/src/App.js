68bc0dfdbe0b3f555c86dded595db0cc1541851e
Vandana Dalal <vanu@Vandanas-MacBook-Pro.local>
2021-06-17 11:05:58 +0530

    alert on UI

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
 import './App.css';
 
 //Redux
@@ -16,9 +17,13 @@ const App = () => (
       <Fragment>
         <Navbar />
         <Route exact path='/' component={Landing} />
-        <section className='container' />
-        <Route exact path='/register' component={Register} />
-        <Route exact path='/login' component={Login} />
+        <section className='container'>
+          <Alert />
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
       </Fragment>
     </Router>
   </Provider>
