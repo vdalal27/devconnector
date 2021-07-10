3d201cc1381ffb00be1db6c6e71e1e96459aff4d
Vandana Dalal <vanu@Vandanas-MacBook-Pro.local>
2021-07-10 15:41:12 +0530

    edit profile from dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profile';
 import Spinner from '../layout/Spinner';
+import DashboardActions from './DashboardActions';
 
 const Dashboard = ({
   getCurrentProfile,
@@ -25,7 +26,9 @@ const Dashboard = ({
       </p>
       <p>
         {profile !== null ? (
-          <Fragment>has</Fragment>
+          <Fragment>
+            <DashboardActions />
+          </Fragment>
         ) : (
           <Fragment>
             <p>
